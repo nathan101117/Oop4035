a072094dd044e2d6f64084a0db7a90f7e9bc0283
nathan101117 <124667960+nathan101117@users.noreply.github.com>
2025-03-12 15:30:22 +0300

    Update example4.js

--- script/example4.js
+++ script/example4.js
@@ -2,7 +2,7 @@ let student = {
   name: "John",
   age: 22,
   greet: function() {
-    return "Hello, my name is $(this.name).";
+    return `Hello, my name is ${this.name}.`;
   }
 };
 console.log(student.greet()); // Hello, my name is John.
